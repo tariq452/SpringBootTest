c71f1db58f8c6e135d58e25e3904d38f98b45ce9
Tariq <tariq.a.tami@gmail.com>
2022-10-19 21:21:16 +0300

    init

---
+++ property-managment/src/main/java/com/ttami/propertymanagment/PropertyManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.ttami.propertymanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyManagmentApplication.class, args);
+	}
+
+}
---
+++ property-managment/src/test/java/com/ttami/propertymanagment/PropertyManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttami.propertymanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
